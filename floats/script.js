a5d49582b79029288c79cdec9af1605519d99d37
BlackWind001 <msanirudh1998@gmail.com>
2021-01-22 00:26:24 +0530

    Initial commit. Floats.

---
+++ floats/script.js
@@ -0,0 +1,9 @@
+let resources = [
+    'https://www.smashingmagazine.com/2018/05/guide-css-layout/#top',
+    'https://www.smashingmagazine.com/2017/12/understanding-css-layout-block-formatting-context/',
+    'https://www.youtube.com/watch?v=LQsjNmkqUOc*/'
+];
+
+for(let resource in resources) {
+    console.log(resources[resource]);
+}
